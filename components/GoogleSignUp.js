5100b36c14f097d0d31984606273a60cbc252eec
Kishan Somaiya <61583134+kishansomaiya@users.noreply.github.com>
2023-08-12 12:25:41 +0530

    Update GoogleSignUp.js

--- components/GoogleSignUp.js
+++ components/GoogleSignUp.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
-import { useRouter } from 'next/router'; 
 
 const GoogleSignUp = ({ onSuccess }) => {
-//   const responseGoogle = (response) => {
-//     onSuccess(response.profileObj);
-//   };
+   const responseGoogle = (response) => {
+     onSuccess(response.profileObj);
+   };
 
-const router = useRouter(); // Initialize the router object
 
-  const responseGoogle = (response) => {
-    if (response.profileObj) {
-      // Successful Google sign-up
-      console.log('Google Sign-Up Response:', response.profileObj);
-
-      // Redirect to dashboard upon successful sign-up
-      router.push('/dashboard');
-    } else {
-      // Error in Google sign-up
-      console.log('Google Sign-Up Error:', response.error);
-    }
-  };
 
   return (
     <GoogleLogin
